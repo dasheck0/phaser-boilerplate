707d8c38916bee612000a250dda74d8c70488f70
Stefan Neidig <stefan@rpdev.net>
2023-09-07 12:32:05 +0200

    fix: make sure default imports are also exported

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
-export * from './config';
+import RegisterPrefab from './decorators/prefab.decorator';
+import FiniteStateMachine, * as GameStateNs from './gamestates/GameState';
+import Sprite, * as SpriteNs from './prefabs/sprite.prefab';
+import AutoLayout, * as AutoLayoutNs from './ui/autolayout.prefab';
+import Image, * as ImageNs from './ui/image.prefab';
+import ImageButton, * as ImageButtonNs from './ui/imageButton.prefab';
+import Text, * as TextNs from './ui/text.prefab';
+import TextButton, * as TextBuuttonNs from './ui/textButton.prefab';
 
+export * from './config';
+export * from './prefabs/PrefabStore';
+export * from './prefabs/base';
+export * from './scenes/BaseScene';
 export * from './types/asset.type';
 export * from './types/math.type';
 export * from './types/prefab.type';
 export * from './types/scene.type';
-
-export * from './scenes/BaseScene';
-
-export * from './prefabs/PrefabStore';
-export * from './prefabs/base';
-export * from './prefabs/sprite.prefab';
-
-export * from './ui/autolayout.prefab';
-export * from './ui/image.prefab';
-export * from './ui/imageButton.prefab';
 export * from './ui/singleImageItem';
-export * from './ui/text.prefab';
-export * from './ui/textButton.prefab';
 export * from './ui/ui';
+export { AutoLayout, AutoLayoutNs, FiniteStateMachine, GameStateNs, Image, ImageButton, ImageButtonNs, ImageNs, RegisterPrefab, Sprite, SpriteNs, Text, TextButton, TextBuuttonNs, TextNs };
 
-export { RegisterPrefab };
-
-  export * from './gamestates/GameState';
-
-import RegisterPrefab from './decorators/prefab.decorator';
